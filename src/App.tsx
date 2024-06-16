d66c27dd6f0a8e4fd119165683a4f13744b2bfdf
Craig McKeachie <craigdmckeachie@gmail.com>
2024-06-15 21:26:47 -0400

    regular bootstrap and bootstrap icons setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,22 @@
-import { useState } from 'react'
-import './App.css'
+// import "./css/bootstrap.min.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+
+import bootstrapIcons from "./assets/bootstrap-icons.svg";
 
 function App() {
-
-
   return (
     <>
-    <h3>Ready</h3>
+      <header>
+        <svg className="bi pe-none me-2" width={32} height={32} fill="#000000">
+          <use xlinkHref={`${bootstrapIcons}#plus`} />
+        </svg>
+      </header>
+      <main>
+        <h3>Ready</h3>
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
