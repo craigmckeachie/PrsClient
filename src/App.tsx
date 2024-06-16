e3c99492b8e343ebfb9e7f58284c81084c49e13a
Craig McKeachie <craigdmckeachie@gmail.com>
2024-06-15 22:36:09 -0400

    header account dropdown

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,21 @@
 // import "./css/bootstrap.min.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+import Header from "./Header";
 
-import bootstrapIcons from "./assets/bootstrap-icons.svg";
-import Button from 'react-bootstrap/Button';
+// import bootstrapIcons from "./assets/bootstrap-icons.svg";
+// import Button from 'react-bootstrap/Button';
 
 function App() {
   return (
     <>
-      <header>
+      {/* <header>
         <Button>Primary</Button>
         <svg className="bi pe-none me-2" width={32} height={32} fill="#000000">
           <use xlinkHref={`${bootstrapIcons}#plus`} />
         </svg>
-      </header>
+      </header> */}
+      <Header/>
       <main>
         <h3>Ready</h3>
       </main>
--- src/Header.tsx
+++ src/Header.tsx
@@ -1,5 +1,66 @@
-function Header(){
-    
+import userProfileImage from "./assets/users/image-34.png";
+import Dropdown from "react-bootstrap/Dropdown";
+import DropdownButton from "react-bootstrap/DropdownButton";
+
+function Header() {
+  return (
+    <header>
+      <div className="navbar bg-body-tertiary py-4 border-bottom">
+        <div className="container-fluid">
+          <a
+            href="/"
+            className="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none"
+          >
+            <svg
+              id="logo-35"
+              width={50}
+              height={39}
+              viewBox="0 0 50 39"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M16.4992 2H37.5808L22.0816 24.9729H1L16.4992 2Z"
+                className="ccompli1"
+                fill="#007AFF"
+              />
+              <path
+                d="M17.4224 27.102L11.4192 36H33.5008L49 13.0271H32.7024L23.2064 27.102H17.4224Z"
+                className="ccustom"
+                fill="#312ECB"
+              />
+            </svg>
+            <span className="small mx-2 fw-semibold">
+              Purchase Request System
+            </span>
+          </a>
+
+          <Dropdown className="me-4">
+            <Dropdown.Toggle
+              as="a"
+              variant="light"
+              className="d-flex align-items-center link-body-emphasis text-decoration-none"
+            >
+              <img
+                src={userProfileImage}
+                alt="profile image"
+                width={32}
+                height={32}
+                className="rounded-circle me-2"
+              />
+              <strong>James Roday</strong>
+            </Dropdown.Toggle>
+            <Dropdown.Menu>
+              <Dropdown.Item href="#">Settings</Dropdown.Item>
+              <Dropdown.Item href="#">Profile action</Dropdown.Item>
+              <Dropdown.Divider />
+              <Dropdown.Item href="#">Sign out</Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+        </div>
+      </div>
+    </header>
+  );
 }
 
 export default Header;
