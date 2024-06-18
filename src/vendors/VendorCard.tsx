33b04a5cacd6aec1fb6b4368bfd4daa3a0542dd3
Craig McKeachie <craigdmckeachie@gmail.com>
2024-06-17 20:58:15 -0400

    use react bootstrap components in vendor card

--- src/vendors/VendorCard.tsx
+++ src/vendors/VendorCard.tsx
@@ -1,5 +1,8 @@
 import bootstrapIcons from "../assets/bootstrap-icons.svg";
 import { IVendor } from "./IVendor";
+import Card from "react-bootstrap/Card";
+import ProgressBar from "react-bootstrap/ProgressBar";
+import Dropdown from "react-bootstrap/Dropdown";
 
 function formatPhoneNumber(phoneNumber: string) {
   const first3Digits = phoneNumber.substring(0, 3);
@@ -8,82 +11,60 @@ function formatPhoneNumber(phoneNumber: string) {
   return `(${first3Digits}) ${middle3Digits}-${last3Digits} `;
 }
 
-interface IVendorCardProps{
-  vendor: IVendor
+interface IVendorCardProps {
+  vendor: IVendor;
 }
 
 function VendorCard({ vendor }: IVendorCardProps) {
   return (
-    <div className="card w-25">
-      <div className="progress">
-        <div
-          className="progress-bar bg-primary"
-          role="progressbar"
-          style={{ width: "60%" }}
-          aria-valuenow={60}
-          aria-valuemin={0}
-          aria-valuemax={100}
-        />
-      </div>
-      <address className="py-4 px-4">
-        <div className="d-flex justify-content-between align-items-center">
-          <span>
-            {" "}
-            <strong>{vendor.name}</strong>{" "}
-            <span className="badge text-bg-secondary">{vendor.code}</span>{" "}
-          </span>
-          <div className="dropdown d-inline">
-            <button
-              className="btn btn-light"
-              style={{ background: "none" }}
-              type="button"
-              data-bs-toggle="dropdown"
-              aria-expanded="false"
-            >
-              <svg
-                className="bi pe-none me-2"
-                width={20}
-                height={20}
-                fill="#007AFF"
+    <>
+      <Card className="w-25">
+        <ProgressBar now={60} />
+        <address className="py-4 px-4">
+          <div className="d-flex justify-content-between align-items-center">
+            <span>
+              <strong>{vendor.name}</strong>{" "}
+              <span className="badge text-bg-secondary">{vendor.code}</span>{" "}
+            </span>
+            <Dropdown className="d-inline">
+              <Dropdown.Toggle
+                as="button"
+                variant="light"
+                className="btn btn-light"
+                style={{ background: "none" }}
               >
-                <use xlinkHref={`${bootstrapIcons}#three-dots-vertical`} />
-              </svg>
-            </button>
-            <ul className="dropdown-menu">
-              <li>
-                <a
-                  href="vendor-edit.html"
-                  className="dropdown-item"
-                  type="button"
+                <svg
+                  className="bi pe-none me-2"
+                  width={20}
+                  height={20}
+                  fill="#007AFF"
                 >
+                  <use xlinkHref={`${bootstrapIcons}#three-dots-vertical`} />
+                </svg>
+              </Dropdown.Toggle>
+              <Dropdown.Menu>
+                <Dropdown.Item as="a" href="#">
                   Edit
-                </a>
-              </li>
-              <li>
-                <button
-                  className="dropdown-item"
-                  type="button"
-                  onClick={() =>
-                    confirm("Are you sure you want to delete this vendor?")
-                  }
-                >
+                </Dropdown.Item>
+                <Dropdown.Item as="a" href="#">
                   Delete
-                </button>
-              </li>
-            </ul>
+                </Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown>
           </div>
-        </div>
-        <br />
-        {vendor.address}
-        <br />
-        {vendor.city}, {vendor.state} {vendor.zip}
-        <br />
-        {/* <abbr title="Phone">P:</abbr> */}
-        {formatPhoneNumber(vendor.phone)}
-        <br />
-        {vendor.email}
-      </address>
-    </div>
+          <br />
+          {vendor.address}
+          <br />
+          {vendor.city}, {vendor.state} {vendor.zip}
+          <br />
+          {/* <abbr title="Phone">P:</abbr> */}
+          {formatPhoneNumber(vendor.phone)}
+          <br />
+          {vendor.email}
+        </address>
+      </Card>
+     
+    </>
   );
 }
 
