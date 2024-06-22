28ef905f5fb9d0017ad4ae5de762fcee3dbd657f
Craig McKeachie <craigdmckeachie@gmail.com>
2024-06-21 20:33:35 -0400

    handle phone number null formatting

--- src/utility/formatUtilities.ts
+++ src/utility/formatUtilities.ts
@@ -1,4 +1,5 @@
 export function formatPhoneNumber(phoneNumber: string) {
+  if(!phoneNumber) return;
   const first3Digits = phoneNumber.substring(0, 3);
   const middle3Digits = phoneNumber.substring(3, 6);
   const last3Digits = phoneNumber.substring(6, 10);
