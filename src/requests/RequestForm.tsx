bafab2847844cc62a02dd3a935cb5757b56a7e21
Craig McKeachie <craigdmckeachie@gmail.com>
2024-06-25 11:55:30 -0400

    request edit works

--- src/requests/RequestForm.tsx
+++ src/requests/RequestForm.tsx
@@ -5,6 +5,8 @@ import { IRequest } from "./IRequest";
 import { requestAPI } from "./RequestAPI";
 import toast from "react-hot-toast";
 import { IUser } from "../users/IUser";
+import { useState } from "react";
+import { userAPI } from "../users/UserAPI";
 
 let emptyRequest: IRequest = {
   id: undefined,
@@ -21,6 +23,14 @@ let emptyRequest: IRequest = {
 function RequestForm() {
   const navigate = useNavigate();
   let { id } = useParams<{ id: string }>();
+  const [users, setUsers] = useState<IUser[]>([]);
+
+  async function loadUsers() {
+    const data = await userAPI.list();
+    console.log(data);
+
+    setUsers(data);
+  }
 
   const {
     register,
@@ -28,6 +38,7 @@ function RequestForm() {
     formState: { errors },
   } = useForm<IRequest>({
     defaultValues: async () => {
+      await loadUsers();
       if (!id) return Promise.resolve(emptyRequest);
       const requestId = Number(id);
       return await requestAPI.find(requestId);
@@ -46,115 +57,185 @@ function RequestForm() {
   // console.log(errors);
 
   return (
-    <form className="d-flex flex-wrap w-75 gap-2" onSubmit={handleSubmit(save)}>
-      <div className="row-1 d-flex flex-row w-100 gap-4">
-        <div className="mb-3 w-75">
-          <label htmlFor="description" className="form-label">
-            Description
-          </label>
-          <input
-            id="description"
-            {...register("description", {
-              required: "Description is required",
-            })}
-            type="text"
-            className={`form-control ${errors?.description && "is-invalid"} `}
-            placeholder="Enter a brief description for your purchase request"
-          />
-          <div className="invalid-feedback">{errors?.description?.message}</div>
-        </div>
-        <div className="mb-3 w-25">
-          <label htmlFor="lastname" className="form-label">
-            Last Name
-          </label>
-          <input
-            id="lastname"
-            {...register("lastname", {
-              required: "Last name is required",
-            })}
-            type="text"
-            className={`form-control ${errors?.lastname && "is-invalid"} `}
-            placeholder="Enter last name"
-          />
-          <div className="invalid-feedback">{errors?.lastname?.message}</div>
-        </div>
-      </div>
-      {/* <div className="row-2 d-flex flex-row w-100 gap-4"></div> */}
-
-      <div className="row-2 d-flex flex-row w-100 gap-4">
-        <div className="mb-3 w-50">
-          <label htmlFor="justification" className="form-label">
-            Justification
-          </label>
-          <input
-            id="justification"
-            {...register("justification")}
-            type="justification"
-            className={`form-control ${errors?.justification && "is-invalid"} `}
-            placeholder="Enter a justification for your purchase request"
-          />
-          <div className="invalid-feedback">{errors?.justification?.message}</div>
-        </div>
-        <div className="mb-3 w-50">
-          <label htmlFor="phone" className="form-label">
-            Phone
-          </label>
-          <input
-            id="phone"
-            {...register("phone")}
-            type="text"
-            className={`form-control ${errors?.phone && "is-invalid"} `}
-            placeholder="Enter phone number"
-          />
-          <div className="invalid-feedback">{errors?.phone?.message}</div>
-        </div>
-      </div>
-      <div className="row-3 d-flex flex-row w-100 gap-4">
-        <div className="mb-3 w-50">
-          <label htmlFor="requestname" className="form-label">
-            Requestname
-          </label>
-          <input
-            id="requestname"
-            {...register("requestname", {
-              required: "Requestname is required",
-              maxLength: {
-                value: 50,
-                message: "Exceeded maximum length",
-              },
-            })}
-            type="text"
-            className={`form-control ${errors?.requestname && "is-invalid"} `}
-            placeholder="Enter requestname"
-          />
-          <div className="invalid-feedback">{errors?.requestname?.message}</div>
-        </div>
-        <div className="mb-3 w-50">
-          <label className="form-label">Role</label>
-          <br />
-
-          <div className="form-check form-check-inline">
-            <input
-              {...register("isReviewer")}
-              type="checkbox"
-              className="form-check-input"
-            />
-            <label htmlFor="isReviewer" className="form-check-label">
-              Reviewer
+    <form className="form" onSubmit={handleSubmit(save)}>
+      <div className="request-header">
+        <div className="column1 w-75">
+          <div className="mb-3">
+            <label htmlFor="description" className="form-label">
+              Description
             </label>
+            <input
+              id="description"
+              {...register("description", {
+                required: "Description is required",
+              })}
+              type="text"
+              className={`form-control ${errors?.description && "is-invalid"} `}
+              placeholder="Enter a brief description for your purchase request"
+            />
+            <div className="invalid-feedback">
+              {errors?.description?.message}
+            </div>
           </div>
-          <div className="form-check form-check-inline">
-            <input
-              {...register("isAdmin")}
-              type="checkbox"
-              className="form-check-input"
-            />
-            <label htmlFor="isAdmin" className="form-check-label">
-              Admin
+          <div className="mb-3">
+            <label htmlFor="justification" className="form-label">
+              Justification
             </label>
+            <input
+              id="justification"
+              {...register("justification", {
+                required: "Justification is required",
+              })}
+              type="text"
+              className={`form-control ${
+                errors?.justification && "is-invalid"
+              } `}
+              placeholder="Enter a justification for your purchase request"
+            />
+            <div className="invalid-feedback">
+              {errors?.justification?.message}
+            </div>
+          </div>
+        </div>
+        <div className="column2 w-50">
+          <div className="mb-3">
+            <label htmlFor="deliveryMode" className="form-label">
+              Delivery Method
+            </label>
+            <select
+              id="deliveryMode"
+              {...register("deliveryMode", {
+                required: "Delivery method is required",
+              })}
+              className={`form-select ${errors?.deliveryMode && "is-invalid"} `}
+            >
+              <option value="">Select...</option>
+              <option value="Pickup">Pickup</option>
+              <option value="Delivery">Delivery</option>
+              <option value="Signature Delivery">Signature Delivery</option>
+            </select>
+            <div className="invalid-feedback">
+              {errors?.deliveryMode?.message}
+            </div>
+          </div>
+          <div className="mb-3">
+            <label htmlFor="status" className="form-label">
+              Status
+            </label>
+            <select
+              id="status"
+              {...register("status", {
+                required: "Status is required",
+              })}
+              disabled
+              defaultValue="New"
+              className={`form-select ${errors?.status && "is-invalid"} `}
+            >
+              <option value="">Select...</option>
+              <option value="New">New</option>
+              <option value="Review">Review</option>
+              <option value="Approved">Approved</option>
+              <option value="Rejected">Rejected</option>
+            </select>
+            <div className="invalid-feedback">{errors?.status?.message}</div>
+          </div>
+          <div className="mb-3">
+            <label htmlFor="userId" className="form-label">
+              Requested By
+            </label>
+            <select
+              id="status"
+              {...register("userId", {
+                required: "Requested by is required",
+              })}
+              disabled
+              // defaultValue={417}
+              className={`form-select ${errors?.userId && "is-invalid"} `}
+            >
+              <option value="">Select...</option>
+              {users.map((u: IUser) => (
+                <option key={u.id} value={u.id}>
+                  {u.firstname} {u.lastname}
+                </option>
+              ))}
+            </select>
+            <div className="invalid-feedback">{errors?.userId?.message}</div>
           </div>
         </div>
       </div>
+      {/* <div className="card p-4">
+        <h5 className="card-title">Items</h5>
+        <table className="table w-75">
+          <thead>
+            <tr>
+              <th>Product</th>
+              <th>Price</th>
+              <th>Quantity</th>
+              <th>Amount</th>
+              <th />
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>
+                <select name id className="form-select">
+                  <option value selected>
+                    Select...
+                  </option>
+                  <option value>
+                    Alpha Soft Touch Gel Stylus Pens (10 pack)
+                  </option>
+                  <option value>College Ruled Notebook</option>
+                </select>
+              </td>
+              <td>
+                <span className="form-label">$0.00</span>
+              </td>
+              <td>
+                <input
+                  type="number"
+                  className="form-control"
+                  defaultValue={100}
+                />
+              </td>
+              <td>
+                <span className="form-label">$0.00</span>
+              </td>
+              <td>
+                <svg
+                  className="bi pe-none me-2"
+                  width={16}
+                  height={16}
+                  fill="#007AFF"
+                >
+                  <use xlinkHref="./images/bootstrap-icons.svg#trash" />
+                </svg>
+              </td>
+            </tr>
+          </tbody>
+          <tfoot>
+            <tr>
+              <td>
+                <button className="btn btn-outline-primary">
+                  <svg
+                    className="bi pe-none me-2"
+                    width={16}
+                    height={16}
+                    fill="#007AFF"
+                  >
+                    <use xlinkHref="./images/bootstrap-icons.svg#plus-circle" />
+                  </svg>
+                  Add a line
+                </button>
+              </td>
+              <td />
+              <td />
+              <td>Total: $0.00</td>
+              <td />
+            </tr>
+          </tfoot>
+        </table>
+      </div> */}
 
       <div className="row-4 d-flex flex-row justify-content-end w-100 gap-4">
         <div className="d-flex justify-content-end mt-4">
